b60063c44fff5e256aa1d71e35dd38acefc79afd
clx0204 <chengzi63@foxmail.com>
2026-03-27 11:13:43 +0800

    Update gps_config.h

---
+++ GPS+手部检测+数据打包/gps_config.h
@@ -0,0 +1,11 @@
+#ifndef GPS_CONFIG_H
+#define GPS_CONFIG_H
+
+// WiFi配置
+#define WIFI_SSID     "你的WiFi名称"
+#define WIFI_PASSWORD "你的WiFi密码"
+
+// 云函数接口地址
+#define CLOUD_FUNCTION_URL "https://your-cloud-function-url"
+
+#endif
